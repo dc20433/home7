4a8799372e776e32ddf0e8333aae48791eada2bc
jz <dc20433@gmail.com>
2026-04-11 22:16:24 -0400

    improved name search to last and/or first name

--- app/controllers/overviews_controller.rb
+++ app/controllers/overviews_controller.rb
@@ -22,25 +22,45 @@ class OverviewsController < ApplicationController
   end
 
   def statistics
-  @total_patients = Regi.count
-  @total_male_patients = Regi.where(gender: "Male").count
-  @total_female_patients = Regi.where(gender: "Female").count
-  @total_other_patients = @total_patients - @total_male_patients - @total_female_patients
-  @total_charts = Chart.count
-  @total_info_records = Patient.count
-  @total_filing = Filing.count
-  @average_charts_per_patient = @total_patients > 0 ? (@total_charts.to_f / @total_patients).round(1) : 0
-  @average_charts_per_day = @total_charts > 0 ? (@total_charts.to_f / (Chart.distinct.count(:t_date))).round(1) : 0
-  @male_records = Patient.joins(:regi).where(regis: {gender: "Male"}).count
-  @female_records = Patient.joins(:regi).where(regis: {gender: "Female"}).count
-  @male_charts = Chart.joins(:regi).where(regis: {gender: "Male"}).count
-  @female_charts = Chart.joins(:regi).where(regis: {gender: "Female"}).count
-  @male_filing = Filing.joins(:regi).where(regis: {gender: "Male"}).count
-  @female_filing = Filing.joins(:regi).where(regis: {gender: "Female"}).count
-  @male_charts_per_patient = @total_male_patients > 0 ? (Chart.joins(:regi).where(regis: {gender: "Male"}).count.to_f / @total_male_patients).round(1) : 0
-  @female_charts_per_patient = @total_female_patients > 0 ? (Chart.joins(:regi).where(regis: {gender: "Female"}).count.to_f / @total_female_patients).round(1) : 0
-  @male_charts_per_day = @total_charts > 0 ? (Chart.joins(:regi).where(regis: {gender: "Male"}).count.to_f / (Chart.joins(:regi).where(regis: {gender: "Male"}).distinct.count(:t_date))).round(1) : 0
-  @female_charts_per_day = @total_charts > 0 ? (Chart.joins(:regi).where(regis: {gender: "Female"}).count.to_f / (Chart.joins(:regi).where(regis: {gender: "Female"}).distinct.count(:t_date))).round(1) : 0
+    @total_patients = Regi.count
+    @total_male_patients = Regi.where(gender: "Male").count
+    @total_female_patients = Regi.where(gender: "Female").count
+    @total_other_patients = @total_patients - @total_male_patients - @total_female_patients
+    @total_charts = Chart.count
+    @total_info_records = Patient.count
+    @total_filing = Filing.count
+    @average_charts_per_patient = @total_patients > 0 ? (@total_charts.to_f / @total_patients).round(1) : 0
+    @average_charts_per_day = @total_charts > 0 ? (@total_charts.to_f / (Chart.distinct.count(:t_date))).round(1) : 0
+    @male_records = Patient.joins(:regi).where(regis: {gender: "Male"}).count
+    @female_records = Patient.joins(:regi).where(regis: {gender: "Female"}).count
+    @male_charts = Chart.joins(:regi).where(regis: {gender: "Male"}).count
+    @female_charts = Chart.joins(:regi).where(regis: {gender: "Female"}).count
+    @male_filing = Filing.joins(:regi).where(regis: {gender: "Male"}).count
+    @female_filing = Filing.joins(:regi).where(regis: {gender: "Female"}).count
+    @male_charts_per_patient = @total_male_patients > 0 ? (Chart.joins(:regi).where(regis: {gender: "Male"}).count.to_f / @total_male_patients).round(1) : 0
+    @female_charts_per_patient = @total_female_patients > 0 ? (Chart.joins(:regi).where(regis: {gender: "Female"}).count.to_f / @total_female_patients).round(1) : 0
+    @male_charts_per_day = @total_charts > 0 ? (Chart.joins(:regi).where(regis: {gender: "Male"}).count.to_f / (Chart.joins(:regi).where(regis: {gender: "Male"}).distinct.count(:t_date))).round(1) : 0
+    @female_charts_per_day = @total_charts > 0 ? (Chart.joins(:regi).where(regis: {gender: "Female"}).count.to_f / (Chart.joins(:regi).where(regis: {gender: "Female"}).distinct.count(:t_date))).round(1) : 0
+    @average_age = Regi.where("dob IS NOT NULL")
+    .where("EXTRACT(YEAR FROM AGE(NOW(), dob)) BETWEEN 5 AND 100")
+    .average("EXTRACT(YEAR FROM AGE(NOW(), dob))")
+    .to_f.round(1)
+    @average_male_age = Regi.where("dob IS NOT NULL")
+    .where("EXTRACT(YEAR FROM AGE(NOW(), dob)) BETWEEN 5 AND 100")
+    .where(gender: "Male")
+    .average("EXTRACT(YEAR FROM AGE(NOW(), dob))")
+    .to_f.round(1)
+    @average_male_age = Regi.where("dob IS NOT NULL")
+    .where("EXTRACT(YEAR FROM AGE(NOW(), dob)) BETWEEN 5 AND 100")
+    .where(gender: "Male")
+    .average("EXTRACT(YEAR FROM AGE(NOW(), dob))")
+    .to_f.round(1)
+    @average_female_age = Regi.where("dob IS NOT NULL")
+    .where("EXTRACT(YEAR FROM AGE(NOW(), dob)) BETWEEN 5 AND 100")
+    .where(gender: "Female")
+    .average("EXTRACT(YEAR FROM AGE(NOW(), dob))")
+    .to_f.round(1)
+
   end
 
   private
