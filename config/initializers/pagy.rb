eb4e9247e8ecf709845f1535295ac9e53d8923dd
jz <dc20433@gmail.com>
2026-05-20 22:24:56 -0400

    fixed pagy.rb errors

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -1,2 +1,12 @@
-require "pagy/extras/bootstrap"
-Pagy::DEFAULT[:items] = 50       # Sets default per-page count to 50
+require 'pagy'
+
+# require "pagy/extras/bootstrap"
+
+# Safe-guard against third-party gems (like pagy-cursor) that freeze Pagy defaults
+if Pagy::DEFAULT.frozen?
+  unfrozen_default = Pagy::DEFAULT.dup
+  Pagy.send(:remove_const, :DEFAULT)
+  Pagy::DEFAULT = unfrozen_default
+end
+
+Pagy::DEFAULT[:items] = 15       # Sets default per-page count to 15
